9966e300700d9d771f0e35b7b3556bf16c21c1a6
romain-chazaut <romain.chazaut@laplateforme.io>
2023-05-29 15:17:01 +0200

    test

---
+++ moduleconnexion.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 29 mai 2023 à 13:15
+-- Version du serveur : 8.0.32
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `moduleconnexion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `login`, `prenom`, `nom`, `password`) VALUES
+(1, 'admin', 'admin', 'admin', '$2y$10$gcy//3EM1NH2oeTHW8mPhuEFCe4WFhVpQZURRhST2qoKHnrhwrsou'),
+(34, 'clacladu69', 'Clarisse', 'Rolletos', '$2y$10$XcssSiv9tKHDQFwYg1VwreoYOvw.WeGh66vChWFYSH3lgAbTNvv4G'),
+(35, 'Romain', 'Romain', 'Romain', '$2y$10$Lufgh.Md98AdUntfG5.YUOGd7sF23wcINr/vneEylZE0MisoG.00.'),
+(36, 'Romain-07', 'Romain', 'Chazaut', '$2y$10$tjx0uRUYfXNcnT635BDouOVCurZIoEVF58IEn.f0mxfM5GW7YLV/O');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
